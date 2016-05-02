b6181d908a331781afb2ff0045c99c6e30ccc3cc
wan <nishikawa0228@sj9.so-net.ne.jp>
2016-05-03 02:00:07 +0900

    remove some files

--- tempra/main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-from core import deploy
-
-
-@deploy
-def main():
-    return ["tempraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!!", "ramen!!", "sushi!!"]
--- tempra/parser/parser.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from ..exceptions import ParseError
-
-
-class TempraParser(object):
-
-    def __init__(self, *args, **kwargs):
